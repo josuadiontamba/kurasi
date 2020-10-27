7a836586a06f1aa9f3fcb0ddbffd341276d15496
Josua Dion Tamba <37044367+josuadiontamba@users.noreply.github.com>
2020-10-27 18:32:36 +0700

    Update settings.py

--- mamawebsite/settings.py
+++ mamawebsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tufa*@h0dw+nydcl!0ry-fo54h#sgg#e5(y2u6!kw&*0@!=(qj'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
